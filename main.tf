138e65acfe92e60cc9aa16c13afbeddddcdb1793
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-03-11 17:18:14 +0530

    Updated th main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,13 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAZQ3DQ4GWR6UV76WT"
-  secret_key = "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"
+  access_key = "AKIAZQ3DQ4GWRK6N3C4W"
+  secret_key = "lTMA+tyXuRxBP8n770l/mcmjqLEQ8YKRaREYpfnm"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "env0" {
   ami           = "ami-07d9b9ddc6cd8dd30" 
   instance_type = "t2.micro"
   tags = {
-    Name = "my-env"
+    Name = "env0-1"
   }
 }
-
-
-
