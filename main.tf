f2db02378e93d815906422e037212c49d415388e
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-03-11 17:32:50 +0530

    Update buck main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,10 @@
 provider "aws" {
   region     = "us-east-1"
   access_key = "AKIAZQ3DQ4GWRK6N3C4W"
-  secret_key = "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"
+  secret_key = "tyXuRxBP8n770l/mcmjqLEQ8YKRaREYpfnm"
 }
 
-resource "aws_instance" "env0" {
-  ami           = "ami-07d9b9ddc6cd8dd30" 
-  instance_type = "t2.micro"
-  tags = {
-    Name = "env0-1"
-  }
+resource "aws_s3_bucket" "s3-demo" {
+  bucket = "buck-sak" 
+  acl    = "private"  
 }
