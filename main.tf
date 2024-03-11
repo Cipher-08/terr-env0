198ba513718caccf36b62bac0ed797317c8a325d
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-03-11 17:55:36 +0530

    Update null main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,5 @@
-provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIAZQ3DQ4GWRK6N3C4W"
-  secret_key = "tyXuRxBP8n770l/mcmjqLEQ8YKRaREYpfnm"
-}
-
-resource "aws_s3_bucket" "s3-demo" {
-  bucket = "buck-sak" 
-  acl    = "private"  
+resource "null_resource" "example" {
+  provisioner "local-exec" {
+    command = "echo Hello, Terraform!"
+  }
 }
