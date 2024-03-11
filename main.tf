aa69a5d204248067f4303608ea73551190de4ef4
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-03-11 17:03:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,16 @@
 provider "aws" {
-  region = "us-east-1"
-
+  region     = "us-east-1"
   access_key = "AKIAZQ3DQ4GWR6UV76WT"
   secret_key = "z9CvRaZhTtx3NL9lbE5mU4igrbt0X//JANyz/EJy"
 }
 
-resource "aws_instance" "ec1" {
-  ami           = "ami-07d9b9ddc6cd8dd30"  
+resource "aws_instance" "example" {
+  ami           = "ami-07d9b9ddc6cd8dd30" 
   instance_type = "t2.micro"
   tags = {
     Name = "my-env"
   }
 }
+
+
 
