f0f79d8490d9247c066bf9826b77ad8d49a8573b
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-03-11 16:53:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "ec1" {
-  ami           = "ami-0ce2cb35386fc22e9"  
+  ami           = "ami-07d9b9ddc6cd8dd30"  
   instance_type = "t2.micro"
   tags = {
     Name = "my-env"
